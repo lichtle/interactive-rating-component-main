f56725bb7ad0cd4337659a2396df28065ab6ca80
Leticia Negreiros <leticia.negreiros@outlook.com>
2023-01-17 21:21:45 -0300

    Adiciona mais comentários no JS

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,14 +2,13 @@ let ratingButtons = document.querySelectorAll(".number");
 let submitButton = document.querySelector(".submit-btn");
 let html = document.querySelector(".rating-box");
 
-ratingButtons.forEach(function (item) {
-  item.addEventListener("click", function () {
-    item.classList.add("active-number"); // Adiciona o active state ao clicar em um botão
-    ratingButtons.forEach(function (item) {
-      // Ao clicar novamente, percorre cada botão novamente
-      item.classList.remove("active-number"); // Removo a classe quando o botão é clicado novamente
+ratingButtons.forEach(function (item) { // Como estamos usando o forEach, podemos utilizar o parâmetro "item" para representar o item atual, como se fosse uma variável
+  item.addEventListener("click", function () { // Para cada item clicado, adiciona um escutador de eventos que executa uma ação
+    item.classList.add("active-number"); // Adiciona o active state ao número clicado
+    ratingButtons.forEach(function (item) { // Para impedir que mais de um número esteja selecionado ao mesmo tempo, o clique percorre o array novamente para remover a classe do novo número clicado, mas isso automaticamente também remove o active state dele, resultando em mudança nenhuma. Para isso precisamos adicionar o item.classList.add("active-number") novamente fora do forEach (mas dentro da função de clique). Poderíamos omitir o item.classList.add("active-number") na linha 7 deste código, mas por questões didáticas resolvi deixar, para entendermos que o active state está sendo adicionado ao clicar no item. É necessário
+      item.classList.remove("active-number"); // Dessa forma estamos adicionando e removendo a classe do item clicado, o que resulta em mudança 
     });
-    item.classList.add("active-number"); // Como a função acima está adicionando e removendo o active state, é necessário adicioná-lo novamente fora da função
+    item.classList.add("active-number"); // Como a função acima está adicionando e removendo o active state, é necessário adicionar a classe novamente fora da função
   });
 });
 
